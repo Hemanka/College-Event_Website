3709afb700837b0824b7d69e2b2f18b72bd3e4c3
Hemanka <hkanoujia02@gmail.com>
2023-04-17 11:48:12 -0400

    css stuff for create rso page

--- create_rso_page.php
+++ create_rso_page.php
@@ -22,61 +22,62 @@
         ?>
     </head>
 
-    <body>
-        <!-- <div class="info"> -->
+    <body class="form_page">
+        
+        <div class="get_info_form">
 
-        <form action="create_rso.php" method="post">
-            <h1>Create New RSO</h1>
-            
-            <div class="login_form_text">
+            <form class="create_form" action="create_rso.php" method="post">
+                <h1 class="form_title">Create New RSO</h1>
+                
+                <div class="form_content">
 
-                <div class="error_message">
-                    <?php if (isset($_SESSION['error_message'])) { ?>
-                        <p><?php echo $_SESSION['error_message'];?></p>
-                    <?php
-                            unset($_SESSION['error_message']);
-                        }
-                    ?>
+                    <div class="error_message">
+                        <?php if (isset($_SESSION['error_message'])) { ?>
+                            <p><?php echo $_SESSION['error_message'];?></p>
+                        <?php
+                                unset($_SESSION['error_message']);
+                            }
+                        ?>
+                    </div>
+
+                    <label for="new_rso_name">RSO Name: </label>
+                    <input type="text" id="new_rso_name" name="new_rso_name" required>
+
+                    <br><br><br>
+
+                    <label>Please fill in the information needed to create an RSO</label>
+
+                    <br><br>
+
+                    <label for="admin_user_email">Admin Email address: </label>
+                    <input type="text" id="admin_user_email" name="admin_user_email" required>
+
+                    <br><br>
+
+                    <label for="first_user_email">First Member Email address: </label>
+                    <input type="text" id="first_user_email" name="first_user_email" required>
+                    
+                    <br><br>
+
+                    <label for="second_user_email">Second Member Email address: </label>
+                    <input type="text" id="second_user_email" name="second_user_email" required>
+                    
+                    <br><br>
+
+                    <label for="third_user_email">Third Member Email address: </label>
+                    <input type="text" id="third_user_email" name="third_user_email" required>
+                    
+                    <br><br>
+
+                    <label for="fourth_user_email">Fourth Member Email address: </label>
+                    <input type="text" id="fourth_user_email" name="fourth_user_email" required>
+
+                    <br><br>
+
+                    <button class="submit_button" type="submit">Create RSO</button>
                 </div>
-
-                <label for="new_rso_name">RSO Name: </label>
-                <input type="text" id="new_rso_name" name="new_rso_name" required>
-
-                <br><br>
-
-                <label>Please fill in the information needed to create an RSO</label>
-
-                <br><br>
-
-                <label for="admin_user_email">Admin Email address: </label>
-                <input type="text" id="admin_user_email" name="admin_user_email" required>
-
-                <br><br>
-
-                <label for="first_user_email">First Member Email address: </label>
-                <input type="text" id="first_user_email" name="first_user_email" required>
-                
-                <br><br>
-
-                <label for="second_user_email">Second Member Email address: </label>
-                <input type="text" id="second_user_email" name="second_user_email" required>
-                
-                <br><br>
-
-                <label for="third_user_email">Third Member Email address: </label>
-                <input type="text" id="third_user_email" name="third_user_email" required>
-                
-                <br><br>
-
-                <label for="fourth_user_email">Fourth Member Email address: </label>
-                <input type="text" id="fourth_user_email" name="fourth_user_email" required>
-
-                <br><br>
-
-                <button type="submit">Create RSO</button>
-            </div>
-        </form>
-        <!-- </div> -->
+            </form>
+        </div>
         
 
     </body>
